e9ee78f10e2a22f8fb372b8f3d354657c8c8d1d4
gity678 <philipegau6@gmail.com>
2025-06-16 22:03:44 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
+from flask import Flask, send_file
 import os
 
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "🚀 Server is live!"
+
+@app.route('/download')
+def download_zip():
+    return send_file('fake_data.zip', as_attachment=True, mimetype='application/zip')
+
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 10000))  # Render يعطيك PORT
+    port = int(os.environ.get('PORT', 5000))  # Render يحدد المنفذ
     app.run(host='0.0.0.0', port=port)
